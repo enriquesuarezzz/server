a54eb2790c4c27debd9e71de712658e8deb6dbba
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-06-05 13:39:52 +0100

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'server-h9f2c9slm-enriquesuarezzzs-projects.vercel.app',
+          },
+      ],
+    },
+}
 
 export default nextConfig;
