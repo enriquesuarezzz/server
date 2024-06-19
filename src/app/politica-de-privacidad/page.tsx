d1f256d4dd5f3fa4b0df3845a56e25958d818e52
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-06-19 11:44:05 +0100

    no message

---
+++ src/app/politica-de-privacidad/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Depilacion Laser SHR | Aviso Legal',
+  description: 'Aviso Legal',
+}
+
+export default function CookiesPage() {
+  return (
+    <>
+      <h1>Politica Provacidad</h1>
+    </>
+  )
+}
